cb39c28965cc04932b61fc80c8cce8e0b8a2830c
szy <sunzhiye@cumt.edu.cn>
2025-05-05 15:42:30 +0800

    revise get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -3,8 +3,9 @@
 use alloc::sync::Arc;
 
 use crate::{
-    fs::{list_apps, open_file, OpenFlags},
-    mm::{translated_refmut, translated_str},
+    config::PAGE_SIZE,
+    fs::{open_file, OpenFlags},
+    mm::{translated_byte_buffer, translated_refmut, translated_str, MapPermission, VirtAddr},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, TaskStatus,
@@ -123,7 +124,36 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
         "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+
+    let user_satp = current_user_token();
+
+    let mut buffers = translated_byte_buffer(user_satp, ts as *mut u8, 16);
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    match buffers.len() {
+        0 => return -1,
+        1 => {
+            buffers[0][0..8].copy_from_slice(&time_val.sec.to_le_bytes());
+            buffers[0][8..16].copy_from_slice(&time_val.usec.to_le_bytes());
+        },
+        2 => {
+            let first_buffer_len = buffers[0].len();
+            if first_buffer_len >= 8 {
+                buffers[0][0..8].copy_from_slice(&time_val.sec.to_le_bytes());
+                buffers[0][8..first_buffer_len].copy_from_slice(&time_val.usec.to_le_bytes()[0..first_buffer_len-8]);
+                buffers[1][0..16-first_buffer_len].copy_from_slice(&time_val.usec.to_le_bytes()[first_buffer_len-8..]);
+            } else {
+                buffers[0][0..first_buffer_len].copy_from_slice(&time_val.sec.to_le_bytes()[0..first_buffer_len]);
+                buffers[1][0..8-first_buffer_len].copy_from_slice(&time_val.sec.to_le_bytes()[first_buffer_len..]);
+                buffers[1][8-first_buffer_len..16-first_buffer_len].copy_from_slice(&time_val.usec.to_le_bytes());
+            }
+        },
+        _ => return -1,
+    }
+    0
 }
 
 /// YOUR JOB: Implement mmap.
